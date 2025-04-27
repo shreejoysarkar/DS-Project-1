f10f38ac38d9f5ba846f62429992ff4a569c5112
shreejoy <sarkarshreejoy30@gmail.com>
2025-04-27 14:51:22 +0530

    'database'

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from src.DSproject.logger import logging
 from src.DSproject.exception import CustomException
+from src.DSproject.components.data_ingestion import DataIngestion
+from src.DSproject.components.data_ingestion import DataIngestionConfig
+
 import sys
 if __name__ == "__main__":
     logging.info("the execution started")
 
     try:
-        a = 1/0
-
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+        
     except Exception as e:
         logging.info('custom exception')
         raise CustomException(e,sys)

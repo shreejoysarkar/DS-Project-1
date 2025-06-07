e86830935d587f6984c7626eb7cf06a6acc85268
shreejoy <sarkarshreejoy30@gmail.com>
2025-06-07 10:23:34 +0530

    model trainner

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.DSproject.exception import CustomException
 from src.DSproject.components.data_ingestion import DataIngestion
 from src.DSproject.components.data_ingestion import DataIngestionConfig
 from src.DSproject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.DSproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
 import sys
 if __name__ == "__main__":
@@ -13,7 +14,12 @@ if __name__ == "__main__":
         train_data_path , test_data_path = data_ingestion.initiate_data_ingestion()
 
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path , test_data_path)
+        train_arr , test_arr,_= data_transformation.initiate_data_transformation(train_data_path , test_data_path)
+
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
+
     except Exception as e:
         logging.info('custom exception')
         raise CustomException(e,sys)
